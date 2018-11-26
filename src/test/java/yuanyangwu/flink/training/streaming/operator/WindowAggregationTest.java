df838112524d188fe976e9e48e87df6095e07e17
Yuanyang Wu <yuanyang_wu@hotmail.com>
2018-11-26 13:51:15 +0800

    test aggregate window

--- src/test/java/yuanyangwu/flink/training/streaming/operator/WindowAggregationTest.java
+++ src/test/java/yuanyangwu/flink/training/streaming/operator/WindowAggregationTest.java
@@ -1,7 +1,9 @@
 package yuanyangwu.flink.training.streaming.operator;
 
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
@@ -69,6 +71,55 @@ public class WindowAggregationTest {
                 new Tuple2<>("John", 20),
                 new Tuple2<>("Mike", 25),
                 new Tuple2<>("John", 12)),
+                stream);
+    }
+
+    //    orig   timestamp=2018-11-08T13:00 watermark=-9223372036854775808 value=(Mike,10)
+    //    orig   timestamp=2018-11-08T13:00:00.100 watermark=-9223372036854775808 value=(John,20)
+    //    orig   timestamp=2018-11-08T13:00:00.200 watermark=2018-11-08T13:00:00.120 value=(Mike,30)
+    //    orig   timestamp=2018-11-08T13:00:00.300 watermark=2018-11-08T13:00:00.120 value=(Mike,25)
+    //    orig   timestamp=2018-11-08T13:00:00.400 watermark=2018-11-08T13:00:00.300 value=(John,12)
+    //    orig   timestamp=2018-11-08T13:00:00.500 watermark=2018-11-08T13:00:00.300 value=(John,50)
+    //
+    //    window timestamp=2018-11-08T13:00:00.299 watermark=2018-11-08T13:00:00.120 value=(Mike,20.0)
+    //    window timestamp=2018-11-08T13:00:00.299 watermark=2018-11-08T13:00:00.120 value=(John,20.0)
+    //    window timestamp=2018-11-08T13:00:00.599 watermark=2018-11-08T13:00:00.500 value=(Mike,25.0)
+    //    window timestamp=2018-11-08T13:00:00.599 watermark=2018-11-08T13:00:00.500 value=(John,31.0)
+    @Test
+    public void aggregateTest() throws Exception {
+        final SingleOutputStreamOperator<Tuple2<String, Double>> stream = orig
+                .keyBy(0)
+                .window(TumblingEventTimeWindows.of(Time.milliseconds(300L)))
+                .aggregate(new AggregateFunction<Tuple2<String, Integer>, Tuple3<String, Integer, Integer>, Tuple2<String, Double>>() {
+                    @Override
+                    public Tuple3<String, Integer, Integer> createAccumulator() {
+                        return new Tuple3<>("", 0, 0);
+                    }
+
+                    @Override
+                    public Tuple3<String, Integer, Integer> add(Tuple2<String, Integer> value, Tuple3<String, Integer, Integer> accumulator) {
+                        return new Tuple3<>(value.f0, value.f1 + accumulator.f1, accumulator.f2 + 1);
+                    }
+
+                    @Override
+                    public Tuple2<String, Double> getResult(Tuple3<String, Integer, Integer> accumulator) {
+                        return new Tuple2<>(accumulator.f0, ((double)accumulator.f1) / accumulator.f2);
+                    }
+
+                    @Override
+                    public Tuple3<String, Integer, Integer> merge(Tuple3<String, Integer, Integer> a, Tuple3<String, Integer, Integer> b) {
+                        return new Tuple3<>(a.f0, a.f1 + b.f1, a.f2 + b.f2);
+                    }
+                });
+
+        orig.addSink(new LogSink<>("orig  "));
+        stream.addSink(new LogSink<>("window"));
+
+        assertStreamEquals(Arrays.asList(
+                new Tuple2<>("Mike", 20.0),
+                new Tuple2<>("John", 20.0),
+                new Tuple2<>("Mike", 25.0),
+                new Tuple2<>("John", 31.0)),
                 stream);
     }
 }
